35e4f1ddc5bbb8f4b00a279415fe19769f69e5ac
Enzo Firmino <enzo.firmino@etu-umontpellier.fr>
2020-02-26 11:26:36 +0100

    test

--- src/src/Exercice1.java
+++ src/src/Exercice1.java
@@ -1,7 +1,31 @@
 public class Exercice1 {
+    public Exercice1() {
+    }
 
-    public int [] racine(int A, int B){
-        double res = 0;
+    public int[] racine(int A, int 😎 {
+        int dim = B - A + 2;
+        int[] res = new int[dim];
+        if (A < 😎 {
+            for(int i = 0; i < dim; ++i) {
+                for(int j = A; j <= B; ++j) {
+                    res[i] = (int)Math.floor(Math.sqrt((double)j));
+                }
+            }
+        }
+
         return res;
+    }
+
+    public static void main(String[] args) {
+        Exercice1 e = new Exercice1();
+        int[] tab = e.racine(1, 5);
+        int[] var3 = tab;
+        int var4 = tab.length;
+
+        for(int var5 = 0; var5 < var4; ++var5) {
+            int t = var3[var5];
+            System.out.println(t);
+        }
+
     }
 }
