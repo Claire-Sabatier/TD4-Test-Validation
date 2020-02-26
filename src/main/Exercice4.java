aba015825739cfafd23e96c25768680abd5ecd9a
Enzo Firmino <enzo.firmino@etu-umontpellier.fr>
2020-02-26 12:18:15 +0100

    exo4

--- src/main/Exercice4.java
+++ src/main/Exercice4.java
@@ -1,5 +1,14 @@
 public class Exercice4 {
 
+    public String triangle (int a, int b, int c){
+        if(a == b || a == c || c == b ){
+            return "Triangle isocèle";
+        }
+        else if(a == b && a == c && b ==c){
+            return "Triangle équilatéral";
+        }
+        else {return "Triangle scalène";}
 
+    }
 
 }
