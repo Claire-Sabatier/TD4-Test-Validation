631e24fc3723add33392f5b4581c6805a1db0209
Enzo Firmino <enzo.firmino@etu-umontpellier.fr>
2020-02-26 11:23:42 +0100

    test

--- src/test/Exercice1Test.java
+++ src/test/Exercice1Test.java
@@ -1,14 +1,32 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Exercice1Test {
 
-    Exercice1 e = new Exercice1();
+    protected Exercice1 e;
+
+    @Before
+    public void setUp() {
+        e = new Exercice1();
+    }
 
     @Test
     void racine_1_a_5() {
         int[] tab = {1,1,1,2,2};
         assertArrayEquals(tab , e.racine(1, 5));
     }
+
+    @Test
+    void a_inf_b(){
+        try {
+            e.racine(6, 3);
+            Assert.fail("Racine devrait lancer une exception");
+        } catch (Exception exc) {
+            // Rien dans le catch puisque c'est le comportement attendu.
+        }
+    }
+
 }
